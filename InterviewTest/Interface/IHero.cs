9e1dbb48ec2f3f429be717181b299923d2db1ad2
kimocks-netizen <brynetinashechikomo@gmail.com>
2023-01-17 10:58:12 +0200

    evolve should return an array of Heros

---
+++ InterviewTest/Interface/IHero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewTest.Controllers
+{
+    public interface IHero
+    {
+         void evolve(int statIncrease = 5);
+       
+    }
+}
