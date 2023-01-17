c995219dcebc7bdaec800797dab793673487f086
kimocks-netizen <brynetinashechikomo@gmail.com>
2023-01-17 22:00:58 +0200

    some a bit of css added

--- FrontEnd/src/app/components/list/list.component.ts
+++ FrontEnd/src/app/components/list/list.component.ts
@@ -9,6 +9,9 @@ import { ApiService } from 'src/app/service/api.service';
 export class ListComponent implements OnInit {
 
   heroes: any;
+  colorClasses = ["color-class-1", "color-class-2", "color-class-3", "color-class-4"];
+  randomColorClass: string;
+
 
   constructor(public service: ApiService) {
     // this.service.getContactsOfHeros().subscribe(res => {
@@ -16,15 +19,17 @@ export class ListComponent implements OnInit {
     // })
    }
 
+   generateRandomColorClass() {
+    this.randomColorClass = this.colorClasses[Math.floor(Math.random() * this.colorClasses.length)];
+  }
    // in the component, add the api service and get the list of contacts.
   ngOnInit() {
+    this.generateRandomColorClass();
     this.service.getContactsOfHeros().subscribe(res => {
       this.heroes=res;
     });
   }
-  public Evole(action: string, heroName: string){
 
-  }
   evolve(hero: any) {
     // this.service.post(`http://localhost:4201/heroes/${hero.name}/evolve`, {action: 'evolve'}).subscribe(data => {
     //   this.heroes = data;
