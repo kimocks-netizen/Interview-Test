4d2dc362833f6467b11949fd76f0ae0f37e5201e
kimocks-netizen <brynetinashechikomo@gmail.com>
2023-01-17 21:40:37 +0200

    some a bit of css added

--- FrontEnd/src/app/components/list/list.component.ts
+++ FrontEnd/src/app/components/list/list.component.ts
@@ -8,13 +8,26 @@ import { ApiService } from 'src/app/service/api.service';
 })
 export class ListComponent implements OnInit {
 
+  heroes: any;
+
   constructor(public service: ApiService) {
-    this.service.getContactsOfHeros().subscribe(res => {
-      console.log(res)
-    })
+    // this.service.getContactsOfHeros().subscribe(res => {
+    //   console.log(res)
+    // })
    }
 
+   // in the component, add the api service and get the list of contacts.
   ngOnInit() {
+    this.service.getContactsOfHeros().subscribe(res => {
+      this.heroes=res;
+    });
   }
+  public Evole(action: string, heroName: string){
 
+  }
+  evolve(hero: any) {
+    // this.service.post(`http://localhost:4201/heroes/${hero.name}/evolve`, {action: 'evolve'}).subscribe(data => {
+    //   this.heroes = data;
+    // });
+  }
 }
--- FrontEnd/src/app/service/api.service.ts
+++ FrontEnd/src/app/service/api.service.ts
@@ -6,9 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
+  post(arg0: string, arg1: { action: string; }) {
+    throw new Error('Method not implemented.');
+  }
   //readonly BaseURL ="http://localhost:5000/api/";
   readonly BaseURL ="http://localhost:4201/api/";
   constructor(private httpclient: HttpClient) { }
+
   //method to get the contacts from the api located at 'http://localhost:4201/heroes'
   public getContactsOfHeros(): Observable<any[]>{
     return this.httpclient.get<any[]>(this.BaseURL +"Heroes"); //Heroes or heroes
