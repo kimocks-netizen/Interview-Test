038aac46e0071fae50cb97245bd8e9f01c1623e8
kimocks-netizen <brynetinashechikomo@gmail.com>
2023-01-17 11:35:23 +0200

    method in api service to get heroes added

--- FrontEnd/src/app/api.service.ts
+++ FrontEnd/src/app/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  readonly BaseURL ="http://localhost:4201/api/";
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
+  //method to get the contacts from the api located at 'http://localhost:4201/heroes'
+  public getContactsOfHeros(): Observable<any[]>{
+    return this.httpclient.get<any[]>(this.BaseURL +"heroes"); //Heroes or heroes
+  }
 }
--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -1,18 +1,26 @@
 ﻿using System;
+//using InterviewTest.Interface;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public class Hero:IHero
+       
     {
         public string name { get; set; }
         public string power { get; set; }
         public List<KeyValuePair<string, int>> stats {get;set;}
         public void evolve(int statIncrease = 5)
         {
-            
+            List<KeyValuePair<string, int>> arrr = new List<KeyValuePair<string, int>>();
+            foreach(KeyValuePair<string, int>acct in stats)
+            {
+                //var newStats = new KeyValuePair<string, int>(arrr.Key,(int));
+            }
+
+
         }
     }
 }
